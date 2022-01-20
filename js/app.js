4e702fa0bf4ed0a1c37e4dd1884ea6f1a21c70ae
Maximiliano Urtubey <urtubey@gmail.com>
2022-01-20 20:27:31 -0300

    Se completó el código .html con algunas tarjetas, y se ajustaron los estilos del .css

--- js/app.js
+++ js/app.js
@@ -1,13 +1,4 @@
-// // document.querySelector('h2')
-// console.log(document.querySelector('h2'));
-// // document.querySelector('p')
-// console.log(document.querySelector('p'));
-// console.log(document.getElementById('intro'));
-
-// let elem = document.querySelector('#intro');
-// console.log(elem);
-// // elem.textContent = 'El nuevo texto'
-// elem.innerHTML = '<i>Nuevo texto</i>';
+// Acceso
 function fnCancelar() {
 	const user = document.getElementById('usuario');
 	user.value = "";
